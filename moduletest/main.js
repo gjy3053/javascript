04f11efb210328711097239f822ec2592195fcba
Jiyeon <gjy30533@naver.com>
2022-12-26 17:57:21 +0900

    12.26

--- moduletest/main.js
+++ moduletest/main.js
@@ -1,3 +1,7 @@
-import { area } from "./module.js";
-//module("test");
+//import * as md from "./module.js"; //참조변수 생성
+import { module as mod, area } from "./module.js";
+import sq, { circle } from "./moduleone.js";
+mod("test");
 area(10, 20);
+sq();
+circle();
